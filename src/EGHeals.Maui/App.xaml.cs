4812d37f7548e622a73b67491af673c6be2669b2
Ahmed Abdou <Ahmed Abdou@DESKTOP-PL8IF8N>
2025-09-02 12:21:12 +0300

    Maui project and its component class lib have been added

---
+++ src/EGHeals.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EGHeals.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/EGHeals.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EGHeals.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
